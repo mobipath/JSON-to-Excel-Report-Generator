541303d97a2fe1c8e935f4df2f8b6781743e1043
amiashraf <amiashraf@gmail.com>
2022-03-28 12:43:57 +0600

    local py backup added

---
+++ pyReportGenerator/local-backup.py
@@ -0,0 +1,13 @@
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cdddb',
+#         'USER': 'postgres',
+#         'PASSWORD': '053164660',
+#         'HOST': 'localhost', # the missing piece of the puzzle
+#         'PORT': '5432', # optional, I don't need this since I'm using the standard port
+#     }
+# }
+
+
+ALLOWED_HOSTS = ['localhost', '0.0.0.0','13.228.186.25','iofact.com', 'excel.iofact.com','127.0.0.1']
