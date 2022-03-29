ac0124ebaaccfe0cacd34f88cb02fcf77f47506f
raihan <tasiftarikul@gmail.com>
2022-03-29 14:41:08 +0600

    changed statis dir path

--- pyReportGenerator/settings.py
+++ pyReportGenerator/settings.py
@@ -164,6 +164,7 @@ LOGGING = {
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "reportGenerator" ,"static"),
 )
